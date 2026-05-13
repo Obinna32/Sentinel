881ab935dcfa5e1b6de25afa1b471db07935532f
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2026-05-13 11:04:18 +0100

    feat:created a path that creates shipment db

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,14 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+import models, database, squad_service, ai_vision
 app = FastAPI()
 
+@app.post("/create-shipment")
+def create(product: str, amount: float, bank_name: str, acc: str, db: Session = Depends(database.get_db)):
+    shipment = models.Shipment(product_name=product, amount=amount, supplier_bank_name = bank_name, supplier_acc_num=acc)
+    db.add(shipment)
+    db.commit()
+    db.refresh(shipment)
+
+    squad = squad_service.initiate_escrow(amount, shipment.id)
+    return {"id": shipment.id, "payment_url": squad['data']['checkout_url']}
