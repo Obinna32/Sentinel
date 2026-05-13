0e5c513ee32a74efb0f32cff246dd035749c2970
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2026-05-13 10:36:40 +0100

    feat: wrote the code that would interact with the squad api to initiate escrow and also to make payment'

---
+++ backend/squad_service.py
@@ -0,0 +1,29 @@
+import requests
+import os
+
+SQUAD_SECRET_KEY = ""
+URL_BASE = "https://sandbox-api-d.squadco.com"
+
+def initiate_escrow(amount, shipment_id):
+    url = f"{URL_BASE}/transaction/initiate"
+    headers = {"Authorization": f"Bearer {SQUAD_SECRET_KEY}"}
+    payload = {
+        "amount": int(amount * 100), #In kobo
+        "email": "buyer@gmail.com",
+        "currency": "NGN",
+        "initiate_type": "inline",
+        "transaction_ref": f"SENTINEL-{shipment_id}"
+    }
+    return requests.post(url, json=payload, headers=headers).json()
+
+def payout_to_supplier(amount, bank, acc, shipment_id):
+    url = f"{URL_BASE}/transfer"
+    headers = {"Authorization": f"Bearer {SQUAD_SECRET_KEY}"}
+    payload = {
+        "amount": int(amount * 100), #In kobo
+        "bank_name": bank,
+        "account_number": acc,
+        "transaction_ref": f"PAY-{shipment_id}",
+        "remark": "Product Verified. Payment released."
+    }
+    return requests.post(url, json=payload, headers=headers).json()
